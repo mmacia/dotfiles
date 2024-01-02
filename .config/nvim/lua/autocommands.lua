a22505c28a851096de0b785f03daa7f40a19dc09
Moisès Macià <mmacia@gmail.com>
2024-01-02 09:49:08 +0100

    tweak vim autofocus

--- .config/nvim/lua/autocommands.lua
+++ .config/nvim/lua/autocommands.lua
@@ -84,3 +84,37 @@ autocmd({ 'FileType' }, {
     setlocal.spell = true
   end
 })
+
+
+---
+--- Disable focus on special windows
+---
+--
+local ignore_filetypes = { 'neo-tree', 'NvimTree' }
+local ignore_buftypes = { 'nofile', 'prompt', 'popup' }
+
+local augroup = augroup('FocusDisable', { clear = true })
+
+autocmd('WinEnter', {
+  group = augroup,
+  callback = function(_)
+    if vim.tbl_contains(ignore_buftypes, vim.bo.buftype) then
+      vim.w.focus_disable = true
+    else
+      vim.w.focus_disable = false
+    end
+  end,
+  desc = "Disable focus autoresize for BufType",
+})
+
+autocmd('FileType', {
+  group = augroup,
+  callback = function(_)
+    if vim.tbl_contains(ignore_filetypes, vim.bo.filetype) then
+      vim.b.focus_disable = true
+    else
+      vim.b.focus_disable = false
+    end
+  end,
+  desc = "Disable focus autoresize for FileType",
+})
