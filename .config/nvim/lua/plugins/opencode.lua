9f01b8bca8e8203da02b8c1ada86ed54f3ed028a
Moisès Macià <moises@doofinder.com>
2025-12-16 10:24:34 +0100

    remap opencode keys

--- .config/nvim/lua/plugins/opencode.lua
+++ .config/nvim/lua/plugins/opencode.lua
@@ -13,12 +13,9 @@ function M.config()
   vim.opt.autoread = true
 
   -- Recommended/example keymaps
-  vim.keymap.set('n', '<leader>oa', function() opencode.ask('@this: ', { submit = true }) end, { desc = 'Ask about this' })
-  vim.keymap.set('v', '<leader>oa', function() opencode.ask('@this: ', { submit = true }) end, { desc = 'Ask about selection' })
+  vim.keymap.set({'n', 'v'}, '<leader>oa', function() opencode.ask('@this: ', { submit = true }) end, { desc = 'Ask about this' })
+  vim.keymap.set('n', '<leader>ot', function() opencode.toggle() end, { desc = 'Toggle opencode' })
   vim.keymap.set('n', '<leader>o+', function() opencode.prompt('@buffer', { append = true }) end, { desc = 'Add buffer to prompt' })
-  vim.keymap.set('v', '<leader>o+', function() opencode.prompt('@selection', { append = true }) end, { desc = 'Add selection to prompt' })
-  vim.keymap.set('n', '<leader>oe', function() opencode.prompt('Explain @cursor and its context') end, { desc = 'Explain this code' })
-  vim.keymap.set('n', '<leader>on', function() opencode.command('session_new') end, { desc = 'New session' })
   vim.keymap.set('n', '<S-C-u>',    function() opencode.command('messages_half_page_up') end, { desc = 'Messages half page up' })
   vim.keymap.set('n', '<S-C-d>',    function() opencode.command('messages_half_page_down') end, { desc = 'Messages half page down' })
   vim.keymap.set({ 'n', 'v' }, '<leader>os', function() opencode.select() end, { desc = 'Select prompt' })
