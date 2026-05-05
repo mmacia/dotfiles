25588074a65a11064d5c61c8dcff5f61d64428bd
Moisès Macià <mmacia@gmail.com>
2026-05-06 00:16:19 +0200

    configure miss typings

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,8 +2,15 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.g.mapleader = ","
-
 vim.opt.relativenumber = false
 vim.opt.scrolloff = 3
 vim.opt.sidescrolloff = 5
+
+local create_user_command = vim.api.nvim_create_user_command
+
+-- handle mistypings
+create_user_command("WQ", "wq", { bang = true, desc = "Mistyped :wq" })
+create_user_command("Wq", "wq", { bang = true, desc = "Mistyped :wq" })
+create_user_command("W", "w", { bang = true, desc = "Mistyped :w" })
+create_user_command("Q", "q", { bang = true, desc = "Mistyped :q" })
+create_user_command("Qa", "qa", { bang = true, desc = "Mistyped :qa" })
